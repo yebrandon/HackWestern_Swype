35e753a732e3a9a7b16340036d488b4a95bb1798
Truman <56100185+trumanbe01@users.noreply.github.com>
2020-11-21 02:09:57 -0500

    Added Yelp API key and sample function for retrieving Yelp data

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
+import { apisAreAvailable } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, TouchableOpacity, View } from 'react-native';
+import { YELP_API_KEY } from './api-keys.json';
+
+const getYelpData = () => {
+  //GET request
+  fetch('https://api.yelp.com/v3/businesses/search?term=restaurants&location=kingston_ontario', {
+    method: 'GET',
+    //Request Type
+    headers: {
+      'Authorization': 'Bearer ' + YELP_API_KEY
+    },
+  })
+    .then((response) => response.json())
+    //If response is in json then in success
+    .then((responseJson) => {
+      //Success
+      alert(JSON.stringify(responseJson));
+      console.log(responseJson);
+    })
+    //If response is not in json then in error
+    .catch((error) => {
+      //Error
+      alert(JSON.stringify(error));
+      console.error(error);
+    });
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Retrieve sample data" onPress={getYelpData} />
       <StatusBar style="auto" />
     </View>
   );
