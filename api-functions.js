0af69ebf8db284d82b8035e1e00974a46643b0ed
Truman <56100185+trumanbe01@users.noreply.github.com>
2020-11-21 21:41:42 -0500

    Tweaked API functions, changing them from using .then to async for promises

--- api-functions.js
+++ api-functions.js
@@ -21,30 +21,15 @@ export async function getCoordinates() {
 }
 
 export async function getDistance(origin, destination) {
-  //GET request
-  return await fetch('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' + origin.latitude + ',' + origin.longitude + '&destinations=' + destination[0] + ',' + destination[1] + '&key=' + GOOGLE_API_KEY, {
+  const response = await fetch('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' + origin.latitude + ',' + origin.longitude + '&destinations=' + destination[0] + ',' + destination[1] + '&key=' + GOOGLE_API_KEY, {
     method: 'GET',
-    //Request Type
   })
-    .then((response) => response.json())
-    //If response is in json then in success
-    .then((responseJson) => {
-      //Success
-      const distance = responseJson.rows[0].elements[0].duration.text;
-      console.log('Distance: ' + distance);
-      return distance;
-    })
-    //If response is not in json then in error
-    .catch((error) => {
-      //Error
-      alert(JSON.stringify(error));
-      console.error(error);
-    });
-};
+  const body = await response.json()
+  const distance = body.rows[0].elements[0].duration.text;
+  return distance
+}
 
 export async function getRestaurants(location, longitude, latitude, radius, category, price) {
-  //GET request
-
   var link = 'https://api.yelp.com/v3/businesses/search?term=restaurants&radius=' + radius + '&limit=50&open_now=true';
   if (location === '') {
     link = link + '&latitude=' + latitude + '&longitude' + longitude;
@@ -57,61 +42,36 @@ export async function getRestaurants(location, longitude, latitude, radius, cate
   if (price !== '') {
     link = link + '&price' + price;
   }
-  return await fetch(link, {
+  const response = await fetch(link, {
     method: 'GET',
-    //Request Type
     headers: {
       'Authorization': 'Bearer ' + YELP_API_KEY
-    },
+    }
   })
-    .then((response) => response.json())
-    //If response is in json then in success
-    .then((responseJson) => {
-      //Success
-      const restaurants = responseJson.businesses.map((value) => value.id);
-      console.log('Restaurants: ' + restaurants);
-      return restaurants;
-    })
-    //If response is not in json then in error
-    .catch((error) => {
-      //Error
-      alert(JSON.stringify(error));
-      console.error(error);
-    });
-};
+  const body = await response.json()
+  const restaurants = body.businesses.map((value) => value.id);
+  return restaurants
+}
 
 export async function getRestaurantData(id) {
-  //GET request
-  return await fetch('https://api.yelp.com/v3/businesses/' + id, {
+  const response = await fetch('https://api.yelp.com/v3/businesses/' + id, {
     method: 'GET',
-    //Request Type
     headers: {
       'Authorization': 'Bearer ' + YELP_API_KEY
-    },
+    }
   })
-    .then((response) => response.json())
-    //If response is in json then in success
-    .then((responseJson) => {
-      //Success
-      const restaurantData = {
-        name: responseJson.name,
-        url: responseJson.url,
-        phone: responseJson.display_phone,
-        categories: responseJson.categories.map((values) => values.title),
-        rating: responseJson.rating,
-        address: responseJson.location.display_address,
-        longitude: responseJson.coordinates.longitude,
-        latitude: responseJson.coordinates.latitude,
-        photos: responseJson.photos,
-        price: responseJson.price,
-        hours: responseJson.hours[0].open.map((value) => [value.start, value.end, value.day])};
-      console.log('Restaurant data: ' + JSON.stringify(restaurantData));
-      return restaurantData;
-    })
-    //If response is not in json then in error
-    .catch((error) => {
-      //Error
-      alert(JSON.stringify(error));
-      console.error(error);
-    });
-};
+  const body = await response.json()
+  const restaurantData = {
+    name: body.name,
+    url: body.url,
+    phone: body.display_phone,
+    categories: body.categories.map((values) => values.title),
+    rating: body.rating,
+    address: body.location.display_address,
+    longitude: body.coordinates.longitude,
+    latitude: body.coordinates.latitude,
+    photos: body.photos,
+    price: body.price,
+    hours: body.hours[0].open.map((value) => [value.start, value.end, value.day])};
+  return restaurantData
+}
