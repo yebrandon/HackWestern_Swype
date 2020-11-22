1a86671eac97ddd2fa8619fd23acc38abde0c57b
Brandon Ye <57378354+yebrandon@users.noreply.github.com>
2020-11-22 00:09:34 -0500

    frontend mostly fleshed out

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import GetStarted from './GetStarted';
+import Stacks from './Stacks';
+import Selected from './Selected';
+import Expand from './Expand';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return <Selected></Selected>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
